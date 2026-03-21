72f8f9356292a982dc580a994ef832e87c59443b
devKiller59 <devkiller59@gmail.com>
2026-03-20 20:38:38 -0500

    update year of maintenance

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <footer className='footer'>
         <p><b>Powered by:</b> The Rick and Morty API</p>
         <p><b>Designed by:</b> <a href="https://github.com/jhoseb29">@Jhoseb29</a> & <a href="https://github.com/devkiller59">@devKiller59</a></p>
-        <p><b>2022</b></p>
+        <p>{new Date().getFullYear()}</p>
       </footer>
    </div>
   );
